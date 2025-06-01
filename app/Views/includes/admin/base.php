af834e040e8c4707f337766cef736655b784f860
BlackShark <159198973+BlackShark404@users.noreply.github.com>
2025-06-01 21:20:45 +0800

    updated some files in the admin base

--- app/Views/includes/admin/base.php
+++ app/Views/includes/admin/base.php
@@ -21,14 +21,6 @@
     <!-- Main Content -->
     <?= $content ?? '' ?>
 
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css">
-    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
-
     <?php if (isset($additionalScripts)): ?>
         <?= $additionalScripts ?>
     <?php endif; ?>
